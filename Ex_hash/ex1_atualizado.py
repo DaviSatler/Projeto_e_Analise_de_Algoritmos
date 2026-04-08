fe3fd369c7103d6425637e32b4109478e95ea19b
Davi Satler <163440573+DaviSatler@users.noreply.github.com>
2026-04-07 23:54:08 -0300

    Add files via upload

---
+++ Ex_hash/ex1_atualizado.py
@@ -0,0 +1,4 @@
+# É importante que funções hash retornem o mesmo valor de saída quando o mesmo valor de entrada for inserido. Caso contrário, não será possível encontrar o item que foi inserido na tabela hash. Quais dessas funções hash são consistentes: f(x) = 1 f(x) = rand() f(x) = proximo_espaco_vazio() f(x) = len(x)
+
+
+#Resposta: Letra D
